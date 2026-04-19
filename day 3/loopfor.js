8a4f9ad9a0f8978443771708494307e0d738ae54
ADMIN <admin@ADMINs-MacBook-Pro.local>
2026-04-19 17:46:50 +0700

    updet forloop - caudieukien

---
+++ day 3/loopfor.js
@@ -0,0 +1,6 @@
+let i = 0;
+let value = 0;
+for (i; i <= 10; i++) {
+    // console.log("I = ", i);
+    console.log("value: ", (value += i));
+}
--- day 3/syntax_caudieukien.js
+++ day 3/syntax_caudieukien.js
@@ -1,1 +1,27 @@
-console.log("Liên kết thành công rồi kkk");
+var age = 30;
+
+if (age >= 30) {
+    console.log("Đủ tuổi uống rượu");
+} else {
+    console.log("Không đủ tuổi uống rượu");
+}
+
+// ===================================
+var toan = 10;
+var ly = 9.5;
+var tiengAnh = 10;
+var diemTB = (toan + ly + tiengAnh) / 3;
+
+if (diemTB >= 9 && diemTB <= 10) {
+    console.log("Xuất sắc");
+} else if (diemTB >= 8 && diemTB <= 8.9) {
+    console.log("Điểm giỏi");
+} else if (diemTB >= 7 && diemTB <= 7.9) {
+    console.log("Điểm Khá");
+} else if (diemTB >= 5 && diemTB <= 6.9) {
+    console.log("Điểm Trung Bình");
+} else if (diemTB >= 4 && diemTB <= 4.9) {
+    console.log("Điểm Yếu");
+} else {
+    console.log("Điểm Kém");
+}
